16432abc16e9deb3c3aac1360a3c294def716677
wester21 <wester21@AAAAB3NzaC1yc2EAAAADAQABAAACAQCxz0xqKaYUqqYM3McML/3C+eipV5Yr5xdGSEmECHL5zmgqv24GClQziLi6BOi78G1Esz847XWOVr1VOOdiWqajgaZOpuyx5VtSgwY1Da7SrgZBOf30WlVRgYfW86av/jLwbRsY98AvZMY2vyPwxPu1oLQ5RtyyX4132QUtizfKOb8f7iTrUyibxRYqc/RvbrYRFTjy1TPeIxr4A7EpjWuofLQU2dQUfDxGcBKzmwOzo7/gTgJgi48awY52h1/uYxzxK1LN6Zx3m3RMuUZDTENpIITLBu8DlNd6QyyM33eHzZaBNwex08T3m1QgM9og9pY3UuN4RZbI/5P91KLwJephp6eUfrpbv7/zU7El/DRmAPVkYVmvBQBkzfEHlNVCXhkvQJJVx8sUPNxHOiCxztLS1c+/786yuKAFl3+B3ut+g/Uo8B3TeBJCpJaunFetcRF5UkP4Q0FvbKEWpcWwS94n6OuduhxSAU17clZy2MxDx4lXKVsuJM3cs498ZkRTcUdr/Ki/pZ+A3Dc6c1RmE1JH2N7hmrPHJ4QNp84GwXJIVjk3f49LVQgFxW3bOdCI7/v1hfq/XTSHDCXWuuWsKdx8MmsxXklu95gPJ/69hjD9A2viz+SOEVM7pfAu/x8UzooU3Viu8CF7lSKX4a2ZiWW8GLmzjU/8zW6jnxMY65LNtQ>
2020-05-27 12:36:34 +0300

    fix bugs

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import design
 import sys
 from PyQt5 import QtWidgets
 import os
+from PyQt5.QtGui import QIcon, QPixmap
+
 
 
 def sendMess(message):
@@ -11,6 +13,7 @@ def sendMess(message):
 
 
 class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -21,9 +24,17 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.listDay_2.clear()
         self.listDay_3.clear()
         self.listDay_4.clear()
+        self.rain = QPixmap('icon/rain.png')
+        self.cloudl = QPixmap('icon/cloudly.png')
+        self.sun = QPixmap('icon/sun.png')
+        # self.icon_1.setScaledContents(True)
+        # self.icon_1.setPixmap(sun)
         self.pushSearch.clicked.connect(self.setCity)
         self.pushRefresh.clicked.connect(self.refresh)
-
+        run = True
+        if run:
+            self.sendWeather('Київ')
+            run = False
 
     def setCity(self):
         self.city = self.lineSearch.text()
@@ -40,40 +51,63 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.listDay_2.clear()
         self.listDay_3.clear()
         self.listDay_4.clear()
-        timelist = [self.time_1, self.time_2,self.time_3, self.time_4, self.time_5,self.time_6,self.time_7,self.time_8]
-        timelistinf = [self.time_1_inf, self.time_2_inf,self.time_3_inf, self.time_4_inf, self.time_5_inf,self.time_6_inf,self.time_7_inf,self.time_8_inf]
+        timelist = [self.time_1, self.time_2, self.time_3, self.time_4, self.time_5, self.time_6, self.time_7,
+                    self.time_8]
+        timelistinf = [self.time_1_inf, self.time_2_inf, self.time_3_inf, self.time_4_inf, self.time_5_inf,
+                       self.time_6_inf, self.time_7_inf, self.time_8_inf]
+        iconlist = [self.icon_1, self.icon_2, self.icon_3, self.icon_4, self.icon_5, self.icon_6, self.icon_7, self.icon_8]
 
-        for i in timelist:
-            i.clear()
-        for i in timelistinf:
-            i.clear()
+        for i in range(8):
+            timelist[i].clear()
+            timelistinf[i].clear()
+            # iconlist[i].clear()
+        try:
+            wh = weather_app.NowWeather(city)
+            city = wh.getCity()
+            self.listInfo.setText(wh.getNowDate() + '\n' + wh.getNowInfo())
+            self.listCity.setText(city)
+            self.listCelsius.setText(wh.getNowCelsius())
+        except:
+            wh = weather_app.NowWeather('Київ')
+            self.listInfo.setText(wh.getNowDate() + '\n' + wh.getNowInfo())
+            self.listCity.setText(city)
+            self.listCelsius.setText(wh.getNowCelsius())
+            self.lineSearch.setText('Місто не знайдено...')
 
-        wh = weather_app.NowWeather(city)
-        city = wh.getCity()
-        self.listInfo.setText(wh.getNowDate() + '\n' + wh.getNowInfo())
-        self.listCity.setText(city)
-        self.listCelsius.setText(wh.getNowCelsius())
+        if wh.getNowInfo().upper().find('ХМАРН') != -1:
+            self.iconToday.setPixmap(self.cloudl)
+        if wh.getNowInfo().upper().find('ДОЩ') != -1:
+            self.iconToday.setPixmap(self.rain)
+        if wh.getNowInfo().upper().find('ОНЯЧНО') != -1:
+            self.iconToday.setPixmap(self.sun)
         wh = weather_app.DayWeather(city)
-        tm = ['2','5','8','11','14','17','20','23']
-
+        tm = ['2', '5', '8', '11', '14', '17', '20', '23']
         for i in range(8):
             timelist[i].setText(wh.getToday()[0][tm[i]])
             timelistinf[i].setText(wh.getToday()[1][tm[i]] + '\n' + wh.getToday()[2][tm[i]])
+            if wh.getToday()[2][tm[i]].upper().find('ХМАРН') != -1:
+                iconlist[i].setPixmap(self.cloudl)
+            if wh.getToday()[2][tm[i]].upper().find('ДОЩ') != -1:
+                iconlist[i].setPixmap(self.rain)
+            if wh.getToday()[2][tm[i]].upper().find('ОНЯЧНО') != -1:
+                iconlist[i].setPixmap(self.sun)
+
 
         day = wh.getDay(1)
-        self.listDay_1.setText(day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
+        self.listDay_1.setText(
+            day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
 
         day = wh.getDay(2)
-        self.listDay_2.setText(day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
+        self.listDay_2.setText(
+            day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
 
         day = wh.getDay(3)
-        self.listDay_3.setText(day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
+        self.listDay_3.setText(
+            day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
 
         day = wh.getDay(4)
-        self.listDay_4.setText(day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
-
-
-
+        self.listDay_4.setText(
+            day['day'] + '\n' + 'Min: ' + day['min'] + '\n' + 'Max: ' + day['max'] + '\n' + day['inf'])
 
 
 if __name__ == '__main__':
@@ -84,4 +118,3 @@ if __name__ == '__main__':
     window.show()
 
     app.exec_()
-
--- weather_app.py
+++ weather_app.py
@@ -1,33 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
-
+GOOGLE_URL = 'https://www.google.com/search?q=weather+'
 
 class NowWeather:
     def __init__(self, city):
-        self.googurl = 'https://www.google.com/search?q=weather+'
-        page = requests.get(self.googurl + city)
+        page = requests.get(GOOGLE_URL + city)
         self.soup = BeautifulSoup(page.content, 'lxml')
 
+
     def getCity(self):
-        temp = str(self.soup.findAll('span', {'class':'BNeawe tAd8D AP7Wnd'})[0].text).split(' ')[0]
+        try:
+            temp = str(self.soup.findAll('span', {'class': 'BNeawe tAd8D AP7Wnd'})[0].text).split(' ')[0]
+        except:
+            temp = 'Київ,'
         return temp[:-1]
 
     def getNowDate(self):
-        temp = str(self.soup.findAll('div', {'class':'BNeawe tAd8D AP7Wnd'})[0].text).split(' ')
-        size = len(temp)
+        temp = str(self.soup.findAll('div', {'class': 'BNeawe tAd8D AP7Wnd'})[0].text).split(' ')
         result = []
         result.append(temp[0].capitalize())
         result.append(str(temp[1][0:5]))
         return " ".join(result)
 
-
     def getNowInfo(self):
-        temp = str(self.soup.findAll('div', {'class':'BNeawe tAd8D AP7Wnd'})[0].text).split(' ')
+        temp = str(self.soup.findAll('div', {'class': 'BNeawe tAd8D AP7Wnd'})[0].text).split(' ')
         size = len(temp)
         result = []
         result.append(str(temp[1][6:]))
         for x in range(size - 2):
-            result.append(str(temp[x+2]))
+            result.append(str(temp[x + 2]))
         return " ".join(result)
 
     def getNowCelsius(self):
@@ -36,19 +37,19 @@ class NowWeather:
 
 class DayWeather:
     def __init__(self, city):
-        url = 'https://ua.sinoptik.ua/погода-'+city
+        url = 'https://ua.sinoptik.ua/погода-' + city
         page = requests.get(url)
-        self.soup =  BeautifulSoup(page.content, 'lxml')
+        self.soup = BeautifulSoup(page.content, 'lxml')
 
     def getDay(self, day):
-        temp = str(self.soup.findAll('div', {'id':'bd'+str(day)})[0].text).split(' ')
-        temp_1 = str(self.soup.findAll('div', {'class':'weatherIco'})[day-1])
+        temp = str(self.soup.findAll('div', {'id': 'bd' + str(day)})[0].text).split(' ')
+        temp_1 = str(self.soup.findAll('div', {'class': 'weatherIco'})[day - 1])
         inf = ''
         x = 36
         i = temp_1[x]
         while i != '"':
-            inf     += i
-            x+=1
+            inf += i
+            x += 1
             i = temp_1[x]
         result = {
             'day': temp[1],
@@ -59,35 +60,36 @@ class DayWeather:
             'max': temp[10]
         }
         return result
+
     def getToday(self):
-        temp = str(self.soup.findAll('tr', {'class':'gray time'})[0].text).split(' ')
+        temp = str(self.soup.findAll('tr', {'class': 'gray time'})[0].text).split(' ')
         time = {
-            '2':temp[1]+temp[2],
-            '5':temp[4]+temp[5],
-            '8':temp[7]+temp[8],
-            '11':temp[10]+temp[11],
+            '2': temp[1] + temp[2],
+            '5': temp[4] + temp[5],
+            '8': temp[7] + temp[8],
+            '11': temp[10] + temp[11],
             '14': temp[13] + temp[14],
             '17': temp[16] + temp[17],
             '20': temp[19] + temp[20],
             '23': temp[22] + temp[23],
-            }
-        temp = str(self.soup.findAll('tr', {'class':'temperature'})[0].text).split(' ')
+        }
+        temp = str(self.soup.findAll('tr', {'class': 'temperature'})[0].text).split(' ')
         temperature = {
-            '2':temp[1],
-            '5':temp[2],
-            '8':temp[3],
-            '11':temp[4],
-            '14':temp[5],
-            '17':temp[6],
-            '20':temp[7],
-            '23':temp[8]
+            '2': temp[1],
+            '5': temp[2],
+            '8': temp[3],
+            '11': temp[4],
+            '14': temp[5],
+            '17': temp[6],
+            '20': temp[7],
+            '23': temp[8]
         }
         templist = []
         for classNum in range(1, 9):
-            temp = str(self.soup.findAll('td', {'class':'p' + str(classNum)})[1])
+            temp = str(self.soup.findAll('td', {'class': 'p' + str(classNum)})[1])
             num = temp.find('title')
             inf = ''
-            x = num+7
+            x = num + 7
             i = temp[x]
             while i != '"':
                 inf += i
@@ -95,17 +97,18 @@ class DayWeather:
                 i = temp[x]
             templist.append(inf)
         information = {
-            '2':templist[0],
-            '5':templist[1],
-            '8':templist[2],
-            '11':templist[3],
-            '14':templist[4],
-            '17':templist[5],
-            '20':templist[6],
-            '23':templist[7]
+            '2': templist[0],
+            '5': templist[1],
+            '8': templist[2],
+            '11': templist[3],
+            '14': templist[4],
+            '17': templist[5],
+            '20': templist[6],
+            '23': templist[7]
         }
         result = [time, temperature, information]
         return result
+
 
 if __name__ == '__main__':
     day = DayWeather('київ')
